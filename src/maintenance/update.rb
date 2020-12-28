3af79d74e54afe00518f3c0bd99fb0883efa87b3
Brainlet <Brainlet>
2020-12-28 15:30:41 -0800

    added more verbosity to update prompts

--- src/maintenance/update.rb
+++ src/maintenance/update.rb
@@ -10,16 +10,23 @@ module Brainlet
       tasks.each do |task|
         case task
         when "klipper"
+          prompt.say("Updating the klipper source")
           Brainlet::Install::klipper(config)
+          prompt.say("Creating the klipper environment")
           Brainlet::Install::klipper_env(config)
         when "moonraker"
+          prompt.say("Updating the moonraker source")
           Brainlet::Install::moonraker(config)
+          prompt.say("Updating the moonraker environment")
           Brainlet::Install::moonraker_env(config)
         when "ui"
+          prompt.say("Updating the UI source")
           Brainlet::Install::ui(config)
         when "webcam"
+          prompt.say("Updating the webcam source")
           Brainlet::Install::webcam(config)
         when "caddy"
+          prompt.say("Updating the caddy binary")
           Brainlet::Install::caddy(config)
         end
       end
